863591de30754e4ca175df9bbbae128d1ccdf585
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-11-03 02:57:39 -0300

    Hacer una consulta a la base de datos.

--- BD_mysql/main.py
+++ BD_mysql/main.py
@@ -8,15 +8,31 @@ try:
         port=3306,
         user='root',
         password='root',
-        db='test'
+        db='world'
     )
 
     if conexion.is_connected():
         print("Conexión exitosa.")
+        infoserver = conexion.get_server_info()
+        print(f"Información del servidor: {infoserver}")
+        cursor = conexion.cursor()
+        cursor.execute("SELECT database();")
+        registro = cursor.fetchone()
+        print(f"Conectado a la base de datos: {registro}")
+
+        cursor.execute("SELECT * FROM city WHERE CountryCode = 'ARG'")
+        resultados = cursor.fetchall()
+
+        for fila in resultados:
+            # print(fila[1] + " - " + fila[2])
+            print(f"Registro: {fila}")
+        print(f"Total de registros: {cursor.rowcount}")
+
 except Error as ex:
     print("Ha ocurrido un error en la conexión a la base de datos |", ex)
 
 finally:
+
     if conexion.is_connected():
         conexion.close()
         print("La conexión ha finalizado")

df903cefc4833bb0c4905fc011cd242d477d6c44
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-11-03 14:35:13 -0300

    Agregué un Readme, y también hice que se mostraran las filas y probé 'DELETE'.

--- BD_mysql/main.py
+++ BD_mysql/main.py
@@ -16,12 +16,24 @@ try:
         print("")
 
         cursor = conexion.cursor()
-        # cursor.execute("INSERT INTO recetas (nombre, tiempo, vegano) VALUES ('Pizza Napolitana', 15, 1);")
-        sentencia = "UPDATE recetas SET nombre = 'Pizza Muzzarella', tiempo = 50 WHERE idReceta = 13"
+        # sentencia = INSERT INTO recetas (nombre, tiempo, vegano) VALUES ('Pizza Napolitana', 15, 1)"
+        # sentencia = "UPDATE recetas SET nombre = 'Pizza Muzzarella', tiempo = 50 WHERE idReceta = 13"
+        # sentencia = "DELETE FROM recetas WHERE nombre = 'Pizza Muzzarella' AND tiempo = 50"
+        sentencia = "INSERT INTO recetas (nombre, tiempo, vegano) VALUES ('Pizza Calabresa', 35, 0)"
+
         cursor.execute(sentencia)
-        conexion.commit() # Confirma la acción que estamos ejecutando
+
+        conexion.commit()  # Confirma la acción que estamos ejecutando
 
         print("Registro insertado con éxito")
+
+        # Mostrar las filas
+        sentencia = "SELECT * FROM recetas"
+        cursor.execute(sentencia)
+        registros = cursor.fetchall()
+
+        for registro in registros:
+            print(registro)
 
 except Error as ex:
     print("Ha ocurrido un error en la conexión a la base de datos |", ex)
---
+++ Readme.md
@@ -0,0 +1,5 @@
+#Tutorial de MySql con Python
+
+¡Hola! Este repositorio es para practicar con el tutorial de Python de UskoKrum2010
+
+Este es el link de uno de los tutoriales: https://www.youtube.com/watch?v=vlwDmzqYqPI

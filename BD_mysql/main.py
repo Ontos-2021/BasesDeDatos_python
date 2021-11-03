6e90c0f0217dcdad635350f0855f1d0190d76b87
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-11-03 02:28:17 -0300

    Primer conexión a la base de datos

--- BD_mysql/main.py
+++ BD_mysql/main.py
@@ -1,2 +1,17 @@
 import mysql.connector
 
+from mysql.connector import Error
+
+try:
+    conexion = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='root',
+        db='test'
+    )
+
+    if conexion.is_connected():
+        print("Conexión exitosa.")
+except Error:
+    print("Ha ocurrido un error en la conexión a la base de datos")
